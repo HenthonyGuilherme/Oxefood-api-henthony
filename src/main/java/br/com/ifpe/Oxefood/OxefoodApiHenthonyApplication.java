e38811d79ea5d47dfa011062a8ae1a21ab0956c5
aluno <joao@gmail.com>
2023-03-23 19:23:11 -0300

    Novo commit

---
+++ src/main/java/br/com/ifpe/Oxefood/OxefoodApiHenthonyApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.Oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiHenthonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiHenthonyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/Oxefood/OxefoodApiHenthonyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.Oxefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiHenthonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
